80da9b39a250e3ff7157e1334b2f854c08937e2c
olakunle_exdee <saheedogunsolu2016@gmail.com>
2020-10-07 21:37:18 +0100

    bug

--- main.js
+++ main.js
@@ -20,21 +20,21 @@ sr.reveal(".animate-right", {
   origin: "right",
   duration: 1000,
   distance: "25rem",
-  delay: 600,
+  delay: 300,
 });
 
 sr.reveal(".animate-top", {
   origin: "top",
   duration: 1000,
   distance: "25rem",
-  delay: 600,
+  delay: 300,
 });
 
 sr.reveal(".animate-bottom", {
   origin: "bottom",
   duration: 1000,
   distance: "25rem",
-  delay: 600,
+  delay: 300,
 });
 
 let showMenu = false;
